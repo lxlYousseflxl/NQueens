f13f49831508a034ec11d5a46f7a239cd5f3ddc9
Omar <omar_hesham@aucegypt.edu>
2021-02-07 21:56:29 +0200

    ready

---
+++ function.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<stdbool.h>
+#define N 4
+
+#include<stdio.h>
+using namespace std;
+void printSolution(int board[N][N]);
+bool isSafe(int board[N][N], int row, int col);
+bool solveNQUtil(int board[N][N], int col);
+bool solveNQ();
+
